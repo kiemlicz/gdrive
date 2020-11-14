6b7cea65c526980e95eef4be95a006e1e1b7a86c
stanislaw_jakiel <stanislaw.dev@gmail.com>
2020-11-14 18:28:49 +0100

    wip project setup (test) added versioneer

--- setup.py
+++ setup.py
@@ -1,4 +1,5 @@
 import setuptools
+import versioneer
 
 with open("README.md", "r") as fh:
     long_description = fh.read()
@@ -7,7 +8,8 @@ with open('requirements.txt') as f:
 
 setuptools.setup(
     name="gdrive",
-    version="0.0.1",
+    version=versioneer.get_version(),
+    cmdclass=versioneer.get_cmdclass(),
     author="kiemlicz",
     author_email="stanislaw.dev@gmail.com",
     description="Google Drive simple client",
